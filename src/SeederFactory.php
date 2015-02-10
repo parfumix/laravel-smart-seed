6dec889056400dc9476175f5b5e613ad6ac3d438
Radu <radu.bordei89@gmail.com>
2015-02-10 14:46:35 +0200

    adding factory method to SeederFactory which will return an provider instance by alias.

--- src/SeederFactory.php
+++ src/SeederFactory.php
@@ -1,13 +1,25 @@
 <?php namespace LaravelSeed;
 
+use LaravelSeed\Exceptions\SeederException;
+
 class SeederFactory {
 
     /**
+     * Return an provider instance ..
+     *
      * @param $alias
-     * @param array $configuration
+     * @throws SeederException
      */
-    public static function factory($alias, array $configuration = []) {
-        
+    public static function factory($alias) {
+        if(! in_array($alias, array_keys(config('seeds.providers'))))
+            throw new SeederException('Invalid provider selected!');
+
+        $config = config('seeds.providers.' . trim(strtolower($alias)));
+
+        if( !$config['class'] )
+            throw new SeederException('Invalid provider class. Please provide one!');
+
+        return new $config['class']($config);
     }
 
 }
