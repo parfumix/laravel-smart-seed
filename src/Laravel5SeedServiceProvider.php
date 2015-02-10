d21fcbff6a5bda6ffcfc344146820c20a5540261
Radu <radu.bordei89@gmail.com>
2015-02-10 11:12:52 +0200

    adding service provider.

---
+++ src/Laravel5SeedServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace LaravelSeed;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel5SeedServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+
+        /** Publish configuration file . */
+        $this->publishes([
+            realpath(__DIR__.'/../config/configuration.php') => config_path('seeds.php')
+        ], 'config');
+    }
+
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
+
+}
