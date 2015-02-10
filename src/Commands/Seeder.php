a61924024fec27bdf19b661c2ded2168191b6326
Radu <radu.bordei89@gmail.com>
2015-02-10 15:33:39 +0200

    adding functional to make source file . for the moment is available just for yaml provider.

--- src/Commands/Seeder.php
+++ src/Commands/Seeder.php
@@ -1,6 +1,8 @@
 <?php namespace LaravelSeed\Commands;
 
 use Illuminate\Console\Command;
+use LaravelSeed\Exceptions\SeederException;
+use LaravelSeed\Laravel5SeedServiceProvider;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
@@ -40,11 +42,27 @@ class Seeder extends Command {
                 sprintf('Please provider an operation! Use follow commands: %s.', implode(', ', $this->availableArgs))
             );
 
-        if( ! class_exists('App\\' . ucfirst(strtolower($this->option('model')))) )
-            return $this->error('Invalid model class');
+        if( $this->argument('operation') != 'run' )
+            if( ! class_exists('App\\' . ucfirst(strtolower($this->option('model')))) )
+                return $this->error('Invalid model class');
 
+        try {
+            // by default for the moment we will using only yaml provider to parse data from yaml files ..
+            $provider = app(Laravel5SeedServiceProvider::IOC_ALIAS)->factory('yaml');
 
+            if( $this->argument('operation') == 'run' ) {
 
+                // need to be run all of the registered seeds ...
+
+            } elseif( $this->argument('operation') == 'make' ) {
+                if( $file = $provider->makeFile( $this->option('model'), $this->option('model'), $this->option('model') ) ) {
+                    return $this->info(sprintf('File "%s" created successfully!', $file));
+                }
+            }
+
+        } catch(SeederException $e) {
+            return $this->error($e->getMessage());
+        }
     }
 
     /**
@@ -70,5 +88,4 @@ class Seeder extends Command {
             ['path', null, InputOption::VALUE_OPTIONAL, 'An custom path to create yaml seeders.', null],
         ];
     }
-
 }
