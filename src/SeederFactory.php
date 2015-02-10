8d0b9d0d21615950630382b75ef133f1318b390b
Radu <radu.bordei89@gmail.com>
2015-02-10 14:00:41 +0200

    adding factory method.

--- src/SeederFactory.php
+++ src/SeederFactory.php
@@ -1,5 +1,13 @@
 <?php namespace LaravelSeed;
 
 class SeederFactory {
-    
+
+    /**
+     * @param $alias
+     * @param array $configuration
+     */
+    public static function factory($alias, array $configuration = []) {
+        
+    }
+
 }
