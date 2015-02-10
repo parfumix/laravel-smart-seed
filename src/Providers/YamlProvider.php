96e7c3e485bb396eaace5135b60e64a4acf5fc90
Radu <radu.bordei89@gmail.com>
2015-02-10 15:03:47 +0200

    implement makeFile function from ProviderInterface.

--- src/Providers/YamlProvider.php
+++ src/Providers/YamlProvider.php
@@ -5,11 +5,29 @@ use LaravelSeed\Contracts\ProviderInterface;
 class YamlProvider implements ProviderInterface {
 
     /**
+     * @var array
+     */
+    private $config;
+
+    public function __construct(array $config) {
+        $this->config = $config;
+    }
+
+    /**
      * Return an array of data to be parsed ...
      *
      * @return array|mixed
      */
     public function getData() {
-        return [];
+        return ['testdata' => 'testdata'];
+    }
+
+    /**
+     * Make source file ..
+     *
+     * @return bool|mixed
+     */
+    public function makeFile() {
+        return true;
     }
 }
