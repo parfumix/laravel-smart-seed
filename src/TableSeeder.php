f0d26c63ce153036dc1c3d9a0016a19217919b06
Radu <radu.bordei89@gmail.com>
2015-02-11 11:48:06 +0200

    adding functional seeding database.

--- src/TableSeeder.php
+++ src/TableSeeder.php
@@ -2,4 +2,25 @@
 
 class TableSeeder {
 
+    /**
+     * Seed data ...
+     *
+     * @param array $seeds
+     * @return array
+     */
+    public static function seed(array $seeds) {
+        $files = [];
+        array_walk($seeds, function($seed) use(&$files) {
+            $class = $seed['class'];
+
+            array_walk($seed['source'], function($source) use($class) {
+                $classname = 'App\\' . $class;
+                $classname::create($source);
+            });
+
+            $files[] = $class;
+        });
+
+        return $files;
+    }
 }
