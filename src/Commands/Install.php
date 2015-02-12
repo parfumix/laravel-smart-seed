29cfc5663e9d4e4452e40696f2f7b52a01814af1
Radu <radu.bordei89@gmail.com>
2015-02-12 14:38:35 +0200

    adding new install comment, to migrate seed table.

---
+++ src/Commands/Install.php
@@ -0,0 +1,49 @@
+<?php namespace LaravelSeed\Commands;
+
+use Illuminate\Console\Command;
+
+class Install extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'smart:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install smart seeder';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire() {
+
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments() {
+        return [];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions() {
+        return [];
+    }
+
+
+}
