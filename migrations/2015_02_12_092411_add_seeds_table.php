1331989e83fbb074332c0d5d7633435080e3af6e
Radu <radu.bordei89@gmail.com>
2015-02-12 11:38:37 +0200

    add fields for seeds table.

--- migrations/2015_02_12_092411_add_seeds_table.php
+++ migrations/2015_02_12_092411_add_seeds_table.php
@@ -2,27 +2,34 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Schema;
 
 class AddSeedsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('seeds', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('seed');
+            $table->string('env');
+            $table->integer('batch');
+            $table->string('hash');
+            $table->timestamps();
+            $table->index('id');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('seeds');
+    }
 
 }
