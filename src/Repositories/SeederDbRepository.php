16d4db56f4961e6115162cce45f8a2369bbf1f75
Radu Bordei <radu.bordei89@gmail.com>
2015-02-13 13:59:16 +0200

    repository get db manager as param.

--- src/Repositories/SeederDbRepository.php
+++ src/Repositories/SeederDbRepository.php
@@ -1,5 +1,16 @@
 <?php namespace LaravelSeed\Repositories;
 
+use Illuminate\Database\ConnectionResolverInterface;
+
 class SeederDbRepository {
-    
+
+    /**
+     * @var ConnectionResolverInterface
+     */
+    private $manager;
+
+    public function __construct(ConnectionResolverInterface $manager) {
+
+        $this->manager = $manager;
+    }
 }
