577a7c396aa1480d89c26397f1a42a5b4c5ffeed
Radu Bordei <radu.bordei89@gmail.com>
2015-02-13 14:15:24 +0200

    adding methods for dbSeederRepository.

--- src/Repositories/SeederDbRepository.php
+++ src/Repositories/SeederDbRepository.php
@@ -1,8 +1,10 @@
 <?php namespace LaravelSeed\Repositories;
 
 use Illuminate\Database\ConnectionResolverInterface;
+use LaravelSeed\Contracts\RepositoryInterface;
+use LaravelSeed\Exceptions\SeederException;
 
-class SeederDbRepository {
+class SeederDbRepository implements RepositoryInterface {
 
     /**
      * @var ConnectionResolverInterface
@@ -13,4 +15,65 @@ class SeederDbRepository {
 
         $this->manager = $manager;
     }
+
+    /**
+     * Return an connection by alias ..
+     *
+     * @param $alias
+     * @return mixed|void
+     */
+    public function connection($alias = '') {
+
+    }
+
+    /**
+     * Check if seeds table already exists ..
+     */
+    public function isTableExists() {
+        return false;
+    }
+
+    /**
+     * Migrate seeds table ..
+     */
+    public function migrateTable() {
+        if( self::isTableExists() )
+            throw new SeederException('Table already exists!');
+    }
+
+    /**
+     * Remove seeds table ..
+     */
+    public function rollBackTable() {
+        if(! self::isTableExists())
+            return true;
+    }
+
+    /**
+     * Get last batch number by _ENV
+     *
+     * @param $env
+     */
+    public function getLastBatch($env) {
+
+    }
+
+    /**
+     * Get next batch number ..
+     *
+     * @param $env
+     */
+    public function getNextBatch($env) {
+        return self::getLastBatch($env) + 1;
+    }
+
+    /**
+     * Get all seeds by the env ..
+     *
+     * @param $env
+     */
+    public function getSeeds($env) {
+
+    }
+
 }
