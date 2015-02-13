638ff6900b21148f5cdc46f365971c5bfbe92924
Radu Bordei <radu.bordei89@gmail.com>
2015-02-13 17:50:14 +0200

    get table fields.

--- src/Providers/AbstractProvider.php
+++ src/Providers/AbstractProvider.php
@@ -2,4 +2,19 @@
 
 class AbstractProvider {
 
+    /**
+     * Get fields table ..
+     *
+     * @param $table
+     * @return mixed
+     */
+    public static function getFieldsTable($table) {
+        $modelObj = new $table;
+        $fields   = app('db')
+            ->connection()
+            ->getSchemaBuilder()
+            ->getColumnListing( $modelObj->getTable() );
+
+        return $fields;
+    }
 }
