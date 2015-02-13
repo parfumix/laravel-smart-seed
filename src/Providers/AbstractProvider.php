3cdee96d952abd791dc889c1f90f54bb5343ed34
Radu Bordei <radu.bordei89@gmail.com>
2015-02-13 19:39:11 +0200

    adding functional for abstractProvider class.

--- src/Providers/AbstractProvider.php
+++ src/Providers/AbstractProvider.php
@@ -3,6 +3,33 @@
 class AbstractProvider {
 
     /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var string
+     */
+    private $source;
+
+    /**
+     * @var
+     */
+    private $env;
+
+    /**
+     * @param array $config
+     * @param string $source
+     * @param $env
+     */
+    public function __construct(array $config, $source = '', $env) {
+
+        $this->config = $config;
+        $this->source = $source;
+        $this->env = $env;
+    }
+
+    /**
      * Get fields table ..
      *
      * @param $table
@@ -16,5 +43,33 @@ class AbstractProvider {
             ->getColumnListing( $modelObj->getTable() );
 
         return $fields;
+    }
+
+    /**
+     * @param $env
+     */
+    public function setEnv($env) {
+        $this->env = $env;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEnv() {
+        return $this->env;
+    }
+
+    /**
+     * @param $source
+     */
+    public function setSource($source) {
+        $this->source = $source;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSource() {
+        return $this->getSource();
     }
 }
