5f3a834beed555b448dd7af5563f4ad74ef4308a
Radu Bordei <radu.bordei89@gmail.com>
2015-02-14 21:48:29 +0200

    adding function helper to get list of files by _ENV.

--- src/Helpers.php
+++ src/Helpers.php
@@ -3,11 +3,30 @@
 /**
  * Convert data from array to yaml format .
  */
-if( !function_exists('func_name') ) {
+
+if( !function_exists('arrayToYaml') ) {
 
     function arrayToYaml(array $array, $mode = 1) {
         $dumper = new Symfony\Component\Yaml\Dumper;
 
         return $dumper->dump($array, $mode);
+    }
+}
+
+
+/**
+ * Get files from specific path by env ..
+ */
+if( !function_exists('getFilesFromPathByEnv')) {
+
+    function getFilesFromPathByEnv($path, $env = '') {
+        $finder = new Symfony\Component\Finder\Finder;
+        $files  = [];
+        $finder->name('*_' . $env . '*');
+        foreach ($finder->in($path) as $file) {
+            $files[] = $file->getFilename();
+        }
+
+        return $files;
     }
 }
