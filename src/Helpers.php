a4e4ad61d45df019a7625b9e92d27f987423220b
Radu Bordei <radu.bordei89@gmail.com>
2015-02-14 21:42:45 +0200

    adding func arraytoYaml.

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * Convert data from array to yaml format .
+ */
 if( !function_exists('func_name') ) {
-    function func_name() {
 
+    function arrayToYaml(array $array, $mode = 1) {
+        $dumper = new Symfony\Component\Yaml\Dumper;
+
+        return $dumper->dump($array, $mode);
     }
 }
